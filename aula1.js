ae1a5aa3ea4bb382fb834839aba503e7b116f9a0
leonardopaglia-cyber <leonardo.paglia@escola.pr.gov.br>
2026-03-09 08:59:24 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,12 @@
+// comentário
+// const ->  constante (não pode ser alterada)
+//let -> variável local
+// var -> variável global
+let nome = "Leonardo"
+let altura = "1.75"
+let idade = "19"
+let cabelo = "marrom"
+let moleton = "preto"
+console.log("Olá, meu nome é " + nome + "! Posso " + idade + " anos, minha cor de cabelo é " + cabelo + " estou usando moleton " + moleton )
+
+
