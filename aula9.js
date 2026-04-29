4567222b9b5f7c76aac5b09a2be33f91c3476935
leonardopaglia-cyber <leonardo.paglia@escola.pr.gov.br>
2026-04-29 09:36:27 -0300

    Add files via upload

---
+++ aula9.js
@@ -0,0 +1,59 @@
+// Modelo da lista de objetos para o projeto 2:
+const produtos = [
+    {
+        nome: "Lápis",
+        valor: "R$ 3,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "AAAA",
+        codigo: 1234
+    },
+    {
+        nome: "Caneta",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Borracha",
+        valor: "R$ 2,50",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Apontador",
+        valor: "R$ 3,50",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Caderno",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Régua",
+        valor: "R$ 3,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Tesoura",
+        valor: "R$ 8,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+]
