58f84091421efe00d9f4f0c7b85274701f8bd4f8
leonardo <leonardo.paglia@escola.pr.gov.br>
2026-08-18 10:55:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// npm init
+// npm i express
+// para executar: noe index.js
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+
+const arquivoID = JSON.parse(fs.readFileSync("id.json", "utf8"))
+let id = arquivoID 
+
+function atualizarID() {
+    id = id + 1
+    fs.writeFileSync("id.json", JSON.stringify({id :id}), "utf8")
+}
+
+app.post("/produto", (req, res) => {
+    const produto = req.body
+    try {
+        const produtos = JSON.parse(fs.readFileSync("produto.json", "utf8"))
+        produto.id = id
+        produtos.push(produto)
+        fs.writeFileSync("produtos.json", JSON.stringifly(produtos), "utf8")
+        res.status(201).json({ mensagem: "Produto cadastrado!" })
+    } catch (error) {
+        res.status(500).json({ erro: error.message })
+    }
+})
+
+app.listen(port, () => {
+    console.log("API rodando da porta " + port)
+})
